2c32b30fde5c49efde9ecca2e153b32427e7ed88
Ellen Bartling <ellenbartling@gmail.com>
2017-08-01 23:08:09 -0500

    Edit copy on hello page

--- src/javascripts/pages/hi.vue
+++ src/javascripts/pages/hi.vue
@@ -1,17 +1,78 @@
 <template>
   <section id="hi-im-ellen" class="">
-    <div class="body">
+    <div class="hero">
       <div class="personal-image">
         <img src="dist/images/me_and_gunner.png" />
       </div>
       <div class="text">
         <h2 class="title">Hi, I’m Ellen</h2>
-        <h4 class="subtitle">interaction designer + front-end developer</h4>
-        <!-- <p>I’m an <strong>interaction designer</strong> and <strong>front-end web developer</strong> in Austin, TX. 🌮 </p> -->
+        <h4 class="subtitle">user experience designer + developer</h4>
 
-        <p>I help organizations engage their users and reinforce their brands through interactive design and web technology. I'm a huge geek for all things design, cognitive science, user research, economics, front-end web tech, and dogs.</p>
+        <p>I help organizations engage their users and build their brands through <a href="work/at-the-fork">interactive design</a>. I'm a huge geek for all things cognitive science, user research, economics, front-end web tech, design, branding, and dogs.</p>
 
         <p>Currently, I work with an amazing team at an Austin, TX creative agency called <a target="_blank" href="http://www.emergentorder.com">Emergent Order</a> that’s dedicated to making awesome things, including apps, websites, digital magazines, identity + branding redesign, and any kind of video production under the sun.</p>
+
+        <p class="currently-nerding"><img width="40px" src="dist/images/noun_844674.png"><em>Currently nerding out about: <a href="https://www.amazon.com/Blank-Slate-Modern-Denial-Nature-ebook/dp/B000QCTNIM/ref=sr_1_1?ie=UTF8&qid=1500250928&sr=8-1&keywords=the+blank+slate">The Blank Slate</a></em></p>
+      </div>
+    </div>
+    <div class="why-both">
+      <div class="text">
+        <h2 class="title">Why design <i>and</i> develop?</h2>
+        <p>Well, most importantly, getting to grapple with every part of the process, from market research to CSS transitions, is incredibly rewarding, and, personally, I think it's crucial to designing and developing truly awesome experiences. My favorite roles on projects are when I can be the “glue” between teams, and hop into whatever part of the process needs my help, or lend perspective to another team member’s work.</p>
+
+        <p>I'm a huge advocate for being a great generalist: I've found that diving into the deep end of front end web development makes me a better designer, communicator, and advocate for the user. Understanding how routes work makes me a better information architect; understanding how hierarchy and whitespace affect information recall makes me a better developer.</p>
+
+        <p>And after four years in the industry, I've learned that being able to "speak" designer and developer is a magical secret sauce that makes projects more fun, more profitable, and less stressful.</p>
+      </div>
+      <div class="stack">
+        <h5 class="subtitle">Design Stack</h5>
+        <ul class="stack-list">
+          <li>Sketch</li>
+          <li>Photoshop</li>
+          <li>Framer.js</li>
+          <li>Invision</li>
+        </ul>
+        <h5 class="subtitle">Tech Stack</h5>
+        <ul class="stack-list">
+          <li><b>Javscript</b></li>
+          <li>Node</li>
+          <li>jQuery</li>
+          <li>Vue.js</li>
+          <li>Gulp / Webpack</li>
+          <li><b>PHP</b></li>
+          <li>Wordpress</li>
+          <li>Laravel</li>
+          <li>Statamic</li>
+        </ul>
+      </div>
+
+      <div class="stack">
+        <h5 class="subtitle">College</h5>
+        <ul class="stack-list">
+          <li><a href="https://www.ccs.ucsb.edu/what-is-ccs" target="_blank">College of Creative Studies, UCSB</a></li>
+          <li><em>B.A. in English Literature</em></li>
+          <li><em>B.A. in Evolutionary Biology</em></li>
+          <p></p>
+          <p>Before I'd really heard of Cognitive Science, I managed to hack my own CogSci curriculum out of evolutionary biology & English literature classes.</p>
+        </ul>
+        <p></p>
+        <h5 class="subtitle">C.V.</h5>
+        <ul class="stack-list">
+          <li><b>Communications Designer & Assistant</b></li>
+          <li><em>Texas Public Policy Foundation</em></li>
+          <p></p>
+          <li><b>UX + UI Designer</b></li>
+          <li><em>Emergent Order</em></li>
+        </ul>
+      </div>
+      <!-- <div class="bg-image">
+        <img src="dist/images/noun_alien_900180.png">
+      </div> -->
+    </div>
+    <div class="cool-thing-1">
+      <div class="image-with-caption">
+        <img src="dist/images/screen-5.jpg" />
+        <p>Hey look, an app I designed! See the final version at <a target="_blank" href="http://attheforkfilm.com/app">At the Fork Film</a>.</p>
       </div>
     </div>
   </section>
